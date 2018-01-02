2045903709795edaf0d9d79e30fe79a2f8e6733a
27DH <lee_jackson@sina.cn>
2018-01-02 23:01:38 +0800

    add Book entity

---
+++ app/src/main/java/com/dailyart/bookstoreapplication/bookdb/Book.java
@@ -0,0 +1,77 @@
+package com.dailyart.bookstoreapplication.bookdb;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+import java.util.Date;
+
+/**
+ * Created by 27DH on 2018/1/2.
+ */
+
+@Entity
+public class Book {
+    @PrimaryKey
+    private int id;
+    @ColumnInfo(name = "book_name")
+    private String bookName;
+    private String author;
+    @ColumnInfo(name = "public_date")
+    private Date publicDate;
+    private String press;
+    private String isbn;
+    private String category;
+
+    public Book(int id) {
+        this.id = id;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getPublicDate() {
+        return publicDate;
+    }
+
+    public void setPublicDate(Date publicDate) {
+        this.publicDate = publicDate;
+    }
+
+    public String getPress() {
+        return press;
+    }
+
+    public void setPress(String press) {
+        this.press = press;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
