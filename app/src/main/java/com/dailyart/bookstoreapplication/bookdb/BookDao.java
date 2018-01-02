44bf7b23f3e853789a852dc1ac7dfcb12fd0050d
27DH <lee_jackson@sina.cn>
2018-01-02 23:38:33 +0800

    add Book entity

---
+++ app/src/main/java/com/dailyart/bookstoreapplication/bookdb/BookDao.java
@@ -0,0 +1,34 @@
+package com.dailyart.bookstoreapplication.bookdb;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+/**
+ * Created by 27DH on 2018/1/2.
+ */
+@Dao
+public interface BookDao {
+    @Query("SELECT * FROM book")
+    List<Book> loadAll();
+    @Query("SELECT * from book where press = :press")
+    List<Book> loadByPress(String press);
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertBooks(Book... books);
+    @Insert
+    void insertBook(Book book);
+    @Delete
+    void deleteBooks(Book... books);
+    @Delete
+    void deleteBook(Book book);
+    @Update
+    void updateBooks(Book... books);
+    @Update
+    void updateBook(Book book);
+
+}
---
+++ app/src/main/java/com/dailyart/bookstoreapplication/bookdb/BookDatabase.java
@@ -0,0 +1,12 @@
+package com.dailyart.bookstoreapplication.bookdb;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
+
+/**
+ * Created by 27DH on 2018/1/2.
+ */
+@Database(entities = {Book.class}, version = 1)
+public abstract class BookDatabase extends RoomDatabase {
+
+}
