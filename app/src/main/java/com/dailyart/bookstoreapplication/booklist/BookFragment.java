9605305f5cb6432ee906ffc34b7daefc1291109f
27DH <lee_jackson@sina.cn>
2018-01-20 23:21:44 +0800

    write...

---
+++ app/src/main/java/com/dailyart/bookstoreapplication/booklist/BookFragment.java
@@ -0,0 +1,134 @@
+package com.dailyart.bookstoreapplication.booklist;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.dailyart.bookstoreapplication.R;
+import com.dailyart.bookstoreapplication.bookdb.Book;
+
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link BookFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link BookFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BookFragment extends Fragment implements BooksContract.View{
+
+    private BookAdapter bookAdapter;
+    private BooksContract.Presenter bookPresenter;
+
+    public BookFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static BookFragment newInstance() {
+        BookFragment fragment = new BookFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_book, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+    @Override
+    public void setPresenter(BooksContract.Presenter presenter) {
+
+    }
+
+    private static class BookAdapter extends BaseAdapter {
+        private List<Book> books;
+
+        private BookAdapter(List<Book> books) {
+            this.books = books;
+        }
+
+        public void replaceData(List<Book> books) {
+            this.books = books;
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount() {
+            return books.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return books.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            View rowView = view;
+            if (rowView == null) {
+                LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+                rowView = inflater.inflate(R.layout.book_item, viewGroup, false);
+            }
+            return null;
+        }
+    }
+}
